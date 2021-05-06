91e74b1f1ed9f35b0be69490877949879256b0fa
Ash Shah <ashni@uw.edu>
2021-05-06 14:39:39 -0700

    Initial Commit

---
+++ iQuiz/iQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizApp.swift
+//  iQuiz
+//
+//  Created by Ashni Shah on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct iQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4c0bb469f17f63dccdf3565e1f71099ff0261923
Ash Shah <ashni@uw.edu>
2021-05-06 20:49:15 -0700

    initial set up

--- iQuiz/ContentView.swift
+++ iQuiz/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false;
+    let categories =
+        ["Marvel Superheroes": "A category about Marvel Superheroes",
+         "Science": "beakers and what not",
+         "Mathematics": "2 plus 2 is 5 right"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                List {
+                    ForEach(categories.sorted(by: >), id: \.key) { key, value in
+                        VStack(alignment: .leading) {
+                            Text(key)
+                                .font(.title3)
+                                .foregroundColor(.blue)
+                            Text(value)
+                                .font(.body)
+                        }
+                        .padding()
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            self.showAlert = true
+                        }, label: {
+                            Text("settings")
+                        }).alert(isPresented: $showAlert) { () -> Alert in
+                            Alert(title: Text("Settings"), message: Text("Your settings go here"))
+                        }
+                    }
+                }                
+            }
+        }
     }
 }
 
